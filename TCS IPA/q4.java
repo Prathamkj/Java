16ac203102db2097d578001b1324d477462e6797
Pratham Jaulkar <prathamkj21@gmail.com>
2026-04-06 11:17:36 +0530

    Java

--- TCS IPA/q4.java
+++ TCS IPA/q4.java
@@ -51,22 +51,27 @@ public class Main {
 
         for (int i = 0; i < nstr.length; i++) {
             String word = nstr[i];
-
+            // accessing each words
             boolean hasDigit = false;
 
             // check whole word
+            // this loop for whole words...
             for (int j = 0; j < word.length(); j++) {
                 if (Character.isDigit(word.charAt(j))) {
+                    // check if it is digit in word 
                     hasDigit = true;
+                    // then check the digit = true
                     break;
                 }
-            }
+            }   
 
             if (hasDigit) {
                 continue;
+                // if it is digit then skip the word
             }
 
             char ch = word.charAt(word.length() - 1);
+            // Accessing the word last character and print the character 
             System.out.print(ch);
         }
     }
